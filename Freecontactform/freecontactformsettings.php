68c6cde6c749f32dafd14e2b5e4bebc1605c43f0
Michael Thomas <mtmail007@gmail.com>
2018-10-30 22:30:35 +0100

    Create Freecontactform/freecontactformsettings.php

---
+++ Freecontactform/freecontactformsettings.php
@@ -0,0 +1,11 @@
+<?php
+
+$email_to = "auto@rijschoolthomas.com"; // your email address
+$email_subject = "Contactformulier van Rijschool Thomas"; // email subject line
+$thankyou = "html/thankyou.htm"; // thank you page
+
+// if you update the question on the form -
+// you need to update the questions answer below
+$antispam_answer = "25";
+
+?>
